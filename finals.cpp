a1f50167ea3ccf2cb0566af4e8121e68baf467a6
Kaatori <kurtzedrikdy+github@gmail.com>
2024-03-12 08:42:26 +0800

    Add files via upload

---
+++ finals.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+struct studentInfo
+{
+    double studentID;
+    string fullname;
+    // needs birthdate initialization here
+    string address;
+    char gender;
+    char degreeProgram;
+    int yearLevel;
+
+};
+void addRecord()
+{
+    studentInfo student;
+    fopen("student.txt","a");
+    ofstream fout;
+    fout.open("student.txt",ios::app);
+    cout<<"Enter Student ID: ";cin>>student.studentID;
+    cout<<"Enter Full Name: ";cin>>student.fullname;
+    cout<<"Enter Address: ";cin>>student.address;
+    cout<<"Enter Gender: ";cin>>student.gender;
+    cout<<"Enter Degree Program: ";cin>>student.degreeProgram;
+    cout<<"Enter Year Level: ";cin>>student.yearLevel;
+    fout.write((char*)&student,sizeof(student));
+    fout.close();
+}
+void displayAll()
+{
+    studentInfo student;
+    ifstream fin;
+    fin.open("student.txt");
+    fin.read((char*)&student,sizeof(student));
+    while(!fin.eof())
+    {
+        cout<<"Student ID: "<<student.studentID<<endl;
+        cout<<"Full Name: "<<student.fullname<<endl;
+        cout<<"Address: "<<student.address<<endl;
+        cout<<"Gender: "<<student.gender<<endl;
+        cout<<"Degree Program: "<<student.degreeProgram<<endl;
+        cout<<"Year Level: "<<student.yearLevel<<endl;
+        fin.read((char*)&student,sizeof(student));
+    }
+    fin.close();
+}
+main()
+{
+    char choice;
+    do
+    {
+        // Main Menu
+        cout<<"Welcome to Group Programmers Student Information System"<<endl;
+        cout<<"What do you want to do?"<<endl;
+        cout<<"1. Add New Record\n2. Search Record\n3. Display All Records\n4. Display Specific Record\n5. Delete Record\n6. Exit"<<endl;
+        cout<<"Please type your selection: ";cin>>choice;
+
+        switch(choice)
+        {
+        case '1':
+            cout<<"Add New Record"<<endl;
+            addRecord();
+            break;
+        case '2':
+            cout<<"Search Record"<<endl;
+            break;
+        case '3':
+            cout<<"Display All Records"<<endl;
+            displayAll(); 
+            break;
+        case '4':
+            cout<<"Display Specific Record"<<endl;
+            break;
+        case '5':
+            cout<<"Delete Record"<<endl;
+            break;
+        case '6':
+            cout<<"Exit"<<endl;
+            return 0;
+        default:
+            cout<<"Invalid Input! Please try again."<<endl;
+            break;
+        }
+    } while (choice != '6');   
+}
